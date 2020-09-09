91e112eb409f0e235c19cfffb17dade1b2752151
Abdulwahab-AlObaid <alwohbee@gmail.com>
2020-09-10 00:13:23 +0300

    Done

---
+++ playground 1/CW-1-1.playground/Contents.swift
@@ -0,0 +1,19 @@
+
+
+var name = "Abdulwahab H. AlObaid"
+var height = "1.81 cm"
+var age = "17"
+var hobby1 = "basketball"
+var hobby2 = "footabll"
+
+print("My name is \(name)\nmy age is: \(age)")
+
+var friendName = "Mushari"
+var friendHieght = "1.79 cm"
+var friendAge = "17"
+var friendHobby1 = "reading"
+var friendHobby2 = "footabll"
+
+var age_difference = ( 17 - 17)
+print(age_difference)
+
